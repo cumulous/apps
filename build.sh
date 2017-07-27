54f86eaa5e7e2dcdba4424ecff94040e8866acdf
Denis Loginov <dloginov@mit.edu>
2017-07-27 18:36:30 +0000

    Implement image building script
    We build, version-tag and push aws-base and all of apps images.

---
+++ build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e
+
+ROOT_DIR="${PWD}"
+
+LOGIN=$(aws ecr get-login)
+ECR=$(echo ${LOGIN} | sed 's|.*https://||')
+
+$LOGIN
+
+build_image () {
+  local name=$1
+  local repo_name=${STACK_NAME}/${name}
+
+  cd "${ROOT_DIR}/images/${name}"
+
+  aws ecr create-repository --repository-name ${repo_name} | \
+    docker pull ${ECR}/${repo_name}
+  docker build -t ${name} .
+
+  local version=$(docker image inspect ${name} -f '{{ .Config.Labels.Version }}')
+  local repo_tag=${ECR}/${repo_name}:${version}
+  
+  docker tag ${name}:latest ${repo_tag}
+  docker push ${repo_tag}
+
+  cd "${ROOT_DIR}"
+}
+
+build_image aws-base
+
+for app in $(ls images/apps); do
+  build_image apps/${app}
+done
