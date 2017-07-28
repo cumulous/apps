a4e3056c51572ed902d6a70b339c7d6b34a67305
Denis Loginov <dloginov@mit.edu>
2017-07-28 17:48:09 +0000

    Chown data directory and use 'exec' in entrypoint.sh
    Since the data volume mounted by ECS is owned by root:root with
    755 permissions, we have to grant user permissions in the entrypoint
    script, and execute the rest of the script as user afterwards.
    
    We use 'exec su' to achieve that.

--- images/aws-base/entrypoint.sh
+++ images/aws-base/entrypoint.sh
@@ -2,79 +2,92 @@
 
 set -e
 
-PREFIX="$1" && shift
-ARGS="$@"
+export PREFIX="$1" && shift
+export ARGS="$@"
+export DATA_PATH="${DATA_PATH:-/data}"
+export DATA_BUCKET
 
-fifo() {
-  local pipe="/tmp/$1"
-  mkdir -p "$(dirname "${pipe}")"
-  mkfifo "${pipe}"
-  echo "${pipe}"
+USER=user
+
+run() {
+  fifo() {
+    local pipe="/tmp/$1"
+    mkdir -p "$(dirname "${pipe}")"
+    mkfifo "${pipe}"
+    echo "${pipe}"
+  }
+
+  s3cp() {
+    aws s3 cp "$@"
+  }
+
+  s3in() {
+    while true; do
+      s3cp "$1" - --quiet | cat > "$2"
+      break
+    done &
+  }
+
+  s3out() {
+    s3cp - "$2" < "$1" &
+    OUT_PIDS="${OUT_PIDS} $!"
+  }
+
+  s3sync() {
+    aws s3 sync "$1" "$2" &
+    trap "exit 143" INT TERM
+    wait $!
+  }
+
+  s3down() {
+    mkdir -p "$2"
+    if flock 200; then
+      s3sync "$@"
+    fi 200>"$2/.lock"
+  }
+
+  local regex='[^[]*(\[/([^]]*)\]:([dio]))[^[]*'
+  local matches=$(echo "${args}" | sed -rn "s|${regex}|\1;\2;\3\n|gp")
+
+  while read -r match; do
+
+    IFS=';' read arg path mode <<< "${match}"
+
+    case ${mode} in
+      i|o)
+        pipe="$(fifo "${path}")"
+        args="${args/"${arg}"/"'${pipe}'"}"
+        ;;
+      d)
+        args="${args/"${arg}"/"'${data_path}/${path}'"}"
+        ;;
+    esac
+
+    s3url="s3://${data_bucket}/${path}"
+
+    case $mode in
+      i)
+        s3in "${s3url}" "${pipe}"
+        exec 3>"${pipe}" &
+        ;;
+      o)
+        s3out "${pipe}" "${s3url}"
+        ;;
+      d)
+        s3down "${s3url}" "${data_path}/${path}"
+        ;;
+    esac
+  done <<< "${matches}"
+
+  ${PREFIX}${ARGS%--*} &
+
+  trap "kill $!" INT TERM
+  wait $! && wait ${OUT_PIDS} || exit $?
 }
 
-s3cp() {
-  aws s3 cp "$@"
-}
+adduser -S ${USER}
+mkdir -p ${DATA_PATH}
+chown ${USER} ${DATA_PATH}
 
-s3in() {
-  while true; do
-    s3cp "$1" - --quiet | cat > "$2"
-    break
-  done &
-}
-
-s3out() {
-  s3cp - "$2" < "$1" &
-  OUT_PIDS="${OUT_PIDS} $!"
-}
-
-s3sync() {
-  aws s3 sync "$1" "$2" &
-  trap "exit 143" INT TERM
-  wait $!
-}
-
-s3down() {
-  mkdir -p "$2"
-  if flock 200; then
-    s3sync "$@"
-  fi 200>"$2/.lock"
-}
-
-URL_REGEX='[^[]*(\[/([^]]*)\]:([dio]))[^[]*'
-matches=$(echo "${ARGS}" | sed -rn "s|${URL_REGEX}|\1;\2;\3\n|gp")
-
-while read -r match; do
-
-  IFS=';' read arg path mode <<< "${match}"
-
-  case ${mode} in
-    i|o)
-      pipe="$(fifo "${path}")"
-      ARGS="${ARGS/"${arg}"/"'${pipe}'"}"
-      ;;
-    d)
-      ARGS="${ARGS/"${arg}"/"'${DATA_PATH}/${path}'"}"
-      ;;
-  esac
-
-  s3url="s3://${DATA_BUCKET}/${path}"
-
-  case $mode in
-    i)
-      s3in "${s3url}" "${pipe}"
-      exec 3>"${pipe}" &
-      ;;
-    o)
-      s3out "${pipe}" "${s3url}"
-      ;;
-    d)
-      s3down "${s3url}" "${DATA_PATH}/${path}"
-      ;;
-  esac
-done <<< "${matches}"
-
-${PREFIX}${ARGS%--*} &
-
-trap "kill $!" INT TERM
-wait $! && wait ${OUT_PIDS} || exit $?
+export -f run
+exec su ${USER} -s /bin/bash -c run
